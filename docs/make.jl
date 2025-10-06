0128907c3468fe98dce7ddeebb9f2e755f5348d9
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-10-06 07:00:45 -0500

    Setting up Documenter

---
+++ docs/make.jl
@@ -0,0 +1,29 @@
+using HeaderArrayFile
+using Documenter
+
+DocMeta.setdocmeta!(HeaderArrayFile, :DocTestSetup, :(using HeaderArrayFile); recursive=true)
+
+
+const _PAGES = [
+    "API Reference" => ["docs.md"],
+]
+
+
+makedocs(;
+    modules=[HeaderArrayFile],
+    authors="Maros Ivanic and Mitch Phillipson",
+    sitename="HeaderArrayFile.jl",
+    format=Documenter.HTML(;
+        canonical="https://github.com/mivanic/HeaderArrayFile.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=_PAGES
+)
+
+deploydocs(;
+    repo = "github.com/mivanic/HeaderArrayFile.jl",
+    devbranch = "main",
+    branch = "gh-pages",
+    push_preview = true
+)
